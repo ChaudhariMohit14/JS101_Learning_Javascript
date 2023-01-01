70eaf2e1844de9951d67a6d1311df414f8ce9910
unknown <chaudharimohit1410@gmail.com>
2023-01-01 17:17:03 +0530

    second commit

---
+++ 313Conditional-Statements/problem3.js
@@ -0,0 +1,11 @@
+let a=23;
+let b=23;
+if(a>b){
+  console.log("a is greater than b");
+}
+else if(a<b){
+  console.log("b is greater than a");
+}
+else{
+  console.log("both equal");
+}
---
+++ 313Conditional-Statements/problem4.js
@@ -0,0 +1,14 @@
+let stored_username="javascript";
+let stored_password="node";
+
+let inputUsername="java";
+let inputPassword="node";
+
+if(stored_username==inputUsername){
+  if(stored_password==inputPassword){
+    console.log("user can login");
+  }
+}
+else{
+  console.log("user can not login")
+}
