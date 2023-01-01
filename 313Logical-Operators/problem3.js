25f2c36bbccf9e1ce74292726965e90490250898
unknown <chaudharimohit1410@gmail.com>
2023-01-01 17:21:39 +0530

    third commit

---
+++ 313Logical-Operators/problem3.js
@@ -0,0 +1,8 @@
+let character="z";
+
+if(!(character=="a") && !(character=="e") && !(character=="i") && !(character=="o") && !(character=="u")){
+  console.log("Consonant");
+}
+else{
+  console.log("Vowel");
+}
---
+++ 313Logical-Operators/problem4.js
@@ -0,0 +1,13 @@
+let a= 4;
+let b= 2;
+let c=10;
+
+if((a>b)&&(a>c)){
+  console.log("a is Greatest");
+}
+if((b>a)&&(b>c)){
+  console.log("b is Greatest");
+}
+if((c>a)&&(c>b)){
+  console.log("c is Greatest");
+}
